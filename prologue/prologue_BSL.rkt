03851c0f8d9b42a115db36f97c1aa4aafb66263c
Wilson Campusano <wilsoncampusanojorge@gmail.com>
2019-06-01 11:53:15 -0400

    arithmetic and arithmetic

--- prologue/prologue_BSL.rkt
+++ prologue/prologue_BSL.rkt
@@ -2,3 +2,7 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname prologue_BSL) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 (+ 4 2)
+(and (or (= (string-length "hello world")
+            (string->number "11"))
+         (string=? "hello world" "good morning"))
+     (>= (+ (string-length "hello world") 60) 80))
