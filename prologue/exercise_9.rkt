58ddbe77d85b3f34fce4e04d1d954d8ee57db886
Wilson <wilsoncampusanojorge@gmail.com>
2019-06-05 14:21:42 -0400

    exercise 9

---
+++ prologue/exercise_9.rkt
@@ -0,0 +1,11 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise_9) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define in -10)
+
+
+(if (number? in)
+ (if (> in 0) (- in 1) (* -1 in))
+ (if (string?) (string-length in) (if (boolean? in) 10 20 )
+     ) )
+
