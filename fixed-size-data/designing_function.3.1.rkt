ea9b5c00e97c31f7df1e8feb161eb42c59e83710
Wilson <wilsoncampusanojorge@gmail.com>
2019-06-26 16:24:24 -0400

    require 2htdp/image library

--- fixed-size-data/designing_function.3.1.rkt
+++ fixed-size-data/designing_function.3.1.rkt
@@ -5,9 +5,10 @@
 ; computes the area of a square with side len
 ; given: 2, expect 4
 ; giben: 7, expect 49
-(define (area-of-square len )
-  (... len ... ))
-
+;(define (area-of-square len )
+;  (... len ... ))
+(define (area-of-square len)
+  (sqr len))
 
 ;Number String Image -> Image
 ; adds s to img, y pixels from top, 10 pixels to the left
@@ -18,5 +19,7 @@
 ; expected:
 ;      (place-image (text "hello" 10 "red") 10 5 ...)
 ;      where ... is (empty-scene 100 100)
+
+(require 2htdp/image)
 (define (add-image y s img)
   (place-image (text s 10 "red") 10 y img))
