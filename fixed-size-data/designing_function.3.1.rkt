6251f04906d57b9642a215f2189772da8c4dbbf2
Wilson <wilsoncampusanojorge@gmail.com>
2019-06-26 16:14:01 -0400

    designing functions process

---
+++ fixed-size-data/designing_function.3.1.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname designing_function.3.1) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; Number -> Numnber
+; computes the area of a square with side len
+; given: 2, expect 4
+; giben: 7, expect 49
+(define (area-of-square len )
+  (... len ... ))
