61807046977c60d6417affdf355736d13b561d72
Wilson <wilsoncampusanojorge@gmail.com>
2019-06-27 16:06:26 -0400

    designing functions 3.1

---
+++ fixed-size-data/exercices.designing_functions.3.1.rkt
@@ -0,0 +1,14 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercices.designing_functions.3.1) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;exercise 34
+; String -> 1String
+; takes s and return the first character of s
+; given:
+;       "Hello" for s
+; expected:
+;       "H"
+
+(define (string-first s)
+  ... )
+
