f5d867a7cc18dfbe299ff3528b47fb9b448049f2
Wilson <wilsoncampusanojorge@gmail.com>
2019-06-28 09:29:45 -0400

    e35,e36,e37,e38

--- fixed-size-data/exercices.designing_functions.3.1.rkt
+++ fixed-size-data/exercices.designing_functions.3.1.rkt
@@ -22,3 +22,35 @@
 ;       "H"
 (define (string-last s)
   ....)
+
+;exercise 36
+; Image -> Number
+; takes img and return the number of pixels in img
+; given:
+;       (empty-scene 100 100) for img
+; expected:
+;        1000
+(require 2htdp/image)
+
+(define (image-area img)
+  (* (image-height img) (image-width img)))
+
+;exercise 37
+; String -> String
+; takes s and return s without it first character
+; given:
+;       "Hello World" for s 
+; expected:
+;       "ello World"
+(define (string-rest s)
+  .... )
+
+;exercise 38
+; String -> String
+; takes s and return s without its last character
+; given:
+;       "Hello World" for s
+; expected:
+;       "Hello worl"
+(define (string-remove-last s)
+  ...)
